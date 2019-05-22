82685ee918a258e427dac1b65e35d0eb02247f83
Agung <indoside@live.nl>
2019-05-22 22:09:07 +0200

    Text Style and Postion = DONE

--- closed-captions/captions.js
+++ closed-captions/captions.js
@@ -10,18 +10,18 @@ captions[5] = ['13.08','15.26'];
 captions[6] = ['16.68','18.12'];
 captions[7] = ['20.3','21.3'];
 captions[8] = ['22.7', '24'];
-captions[9] = ['47','48.5'];
+captions[9] = ['47','49'];
 captions[10] = ['49.4','51.98'];
 captions[11] = ['52','53'];
 // [grunts]
-captions[12] = ['81.5','82.5'];
-captions[13] = ['86','87'];
+captions[12] = ['81.5','85.5'];
+captions[13] = ['86','90'];
 captions[14] = ['96.2','97.32'];
 // When I'm done, half of humanity will still be alive.
 captions[15] = ['99.12','102.24'];
-captions[16] = ['104.02','105.68'];
+captions[16] = ['104.02','108.68'];
 captions[17] = ['107.9','109.82'];
 // [pained grunt]
-captions[18] = ['111.6','112.68'];
+captions[18] = ['111.6','115.68'];
 captions[19] = ['118.42','119.56'];
 captions[20] = ['120.28','0'];
