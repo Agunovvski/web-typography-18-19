54445fa2023888fe6b228e7385bdf103205d77e6
Agung <indoside@live.nl>
2019-05-23 00:37:23 +0200

    Styles done

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,17 +1,22 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	5.5,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	5.5, // 0
+	22, // 1 boom
+	25.8, // 2 shake
+	28, // 3 shake
+	28.3, // 4 shake
+	32, // 5 shake
+	37, // 6 glow
+	42.5, // 7 another kick before video
+	54, // 8 video 3 play
+	56, // 9 smash
+	61, // 10 shake
+	62.5, // 11 focus thanos
+	65, // 12 glow2
+	74, // 13 shake
+	75, // 14 zwakker (kleiner) worden
+	86 // 15 bloed
 ];
 
 
